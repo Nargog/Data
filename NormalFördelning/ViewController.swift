206a03d59ab5d99508b550e6cc887263dedab576
Mats Hammarqvist <mats.hammarqvist@icloud.com>
2018-02-24 21:17:52 +0100

    Initial Commit

---
+++ NormalFördelning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NormalFördelning
+//
+//  Created by Mats Hammarqvist on 2018-02-24.
+//  Copyright © 2018 Mats Hammarqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
